5f434d565f59caccd4547c8e92a64796d8b75f60
Kyle OBrien <kyleobis@gmail.com>
2024-02-21 00:44:05 -0500

    ISS init

---
+++ src/components/fetch.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+const Fetch = () => {
+  const [coords, setCoords] = useState([]);
+ 
+  useEffect(() => {
+   const fetchData = () => {
+    fetch('http://api.open-notify.org/iss-now.json')
+      .then(response => response.json())
+      .then((data) => { 
+        setCoords(data.iss_position);
+      });
+      //then runs sequenctiall through all the commands
+      //fetch URL
+      //Parse the data
+      //take that data and do something with it
+      //useEffect is a React Hook that lets you synchronize a component with an external system. 
+    }
+    //intial call
+    fetchData();
+    //contining call
+    const intervalId = setInterval(fetchData, 1000);
+    //clean up upon re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    }, []);
+
+  return (
+    <div>
+        <p>ISS Location =====</p>
+        {coords && ( //&& render only if position has data, or truthy
+            <div>
+                <p>latitude: {coords.latitude}</p>
+                <p>longitude: {coords.longitude}</p>
+            </div>
+        )}
+    </div>
+  );
+};
+export default Fetch;
---
+++ src/components/fetchAss.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+const FetchAss = () => {
+  const [astros, setAstros] = useState([]);
+ 
+  useEffect(() => {
+
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then((data) => { 
+        setAstros(data.people);
+      });
+    }, []);
+
+  return (
+    <div>
+        <p>Ass in Space =====</p>
+        {astros && astros.map((astro) => (
+            <div>
+                <p>Name: {astro.name}</p>
+                <p>Spaceship: {astro.craft}</p>
+            </div>
+        ))}
+    </div>
+  );
+};
+export default FetchAss;
