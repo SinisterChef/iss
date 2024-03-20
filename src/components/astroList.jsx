37c797d07c84522fa0a71ac11dd40c2635cbae05
Kyle OBrien <kyleobis@gmail.com>
2024-03-20 16:36:02 -0400

    fixed api security issues

--- src/components/astroList.jsx
+++ src/components/astroList.jsx
@@ -1,20 +1,6 @@
 import { useState, useEffect } from 'react';
-
-const AstroList = () => {
-  const [astros, setAstros] = useState([]);
- 
-  useEffect(() => {
-
-    fetch('http://api.open-notify.org/astros.json')
-      .then(response => response.json())
-      .then((data) => { 
-        setAstros(data.people);
-      });
-    }, []);
-
   return (
     <div>
-        <p>Ass in Space =====</p>
         {astros && astros.map((astro) => (
             <div>
                 <p>Name: {astro.name}</p>
@@ -23,5 +9,5 @@ const AstroList = () => {
         ))}
     </div>
   );
-};
+
 export default AstroList;
