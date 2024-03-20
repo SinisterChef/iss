408e311bf85c428599a5732a5b78f1a3e2443a02
Kyle OBrien <kyleobis@gmail.com>
2024-03-20 17:19:23 -0400

    Moving astro fetch call to app.js and passing it as a prop to clean up the card jsx

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import Map from '../components/Map';
 import AstroCard from '../components/AstroCard';
+import { useState, useEffect } from 'react';
 import './bootstrap.min.css';
 import './App.css';
 
 
-//map compontent
-
 export default function App() {
+
+  const [astros, setAstros] = useState([]);
+//const [portraits, setPortrait] = useState([]);
+ 
+  useEffect(() => {
+
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then((data) => { 
+        console.log(data);
+        setAstros(data.people);
+      });
+    }, []);
+
   return (
     <div>
       <Map />
-      <AstroCard />
+      <AstroCard astros={astros} />
     </div>
   )
 }
