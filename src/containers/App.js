a2ae76bd31a77b446b29a24fef1a686c951eaf57
Kyle OBrien <kyleobis@gmail.com>
2024-03-19 21:36:50 -0400

    Adjusting favicon

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,9 @@ import Fetch from '../components/fetch';
 import AstroCard from '../components/AstroCard';
 import './App.css';
 
+
+//map compontent
+
 export default function App() {
   return (
     <div>
