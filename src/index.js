1007f3e041791462e8b1a867c1c1b7ba4e1aa1e5
Kyle OBrien <kyleobis@gmail.com>
2025-07-10 15:35:19 -0400

    Removed web vitals from index.js import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './containers/App';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
